fcf99ad57c54090412b6686e13725fff73a1b7b1
itamar124812 <itamar124812@gmail.com>
2022-03-24 02:14:54 +0200

    Finish imagga api

--- DALTests/ImaggaApiTests.cs
+++ DALTests/ImaggaApiTests.cs
@@ -14,7 +14,22 @@ namespace DAL.Tests
         [TestMethod()]
         public void getImageTagsTest()
         {
-            ImaggaApi.getImageTags("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg");
+            var results = ImaggaApi.getImageTags("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg");
+            var hresuls = ImaggaApi.getImageTags("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg", "he");
+            for (int i = 0; i < results.Tags.Count; i++)
+            {
+                Console.WriteLine("English: " + results.Tags[i] + " and now in hebrew: " + hresuls.Tags[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void getImageCategoriesTest()
+        {
+            foreach (var item in ImaggaApi.getImageCategories(NasaApi.GetDailyImage(DateTime.Parse("23/03/2022")).Uri).Tags)
+            {
+                Console.WriteLine("uri:" + ImaggaApi.getImageCategories(NasaApi.GetDailyImage(DateTime.Parse("23/03/2022")).Uri));
+                Console.WriteLine(item);
+            } 
         }
     }
 }
